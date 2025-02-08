4e27cbf8786cd9ecf79493c98fdab196c5b2d622
iYEiD <zeinoeid@gmail.com>
2025-02-08 14:04:14 +0200

    feat: add hero dashboard, routing, and navbar

--- src/app/features/components/hero-detail/hero-detail.component.ts
+++ src/app/features/components/hero-detail/hero-detail.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { Hero } from '../../models/hero.model';
-import { Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { HeroService } from '../../../services/hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   standalone: true,
@@ -10,6 +13,27 @@ import { Input } from '@angular/core';
   templateUrl: './hero-detail.component.html',
   styleUrl: './hero-detail.component.scss'
 })
-export class HeroDetailComponent {
-@Input() hero?: Hero;
+export class HeroDetailComponent implements OnInit {
+
+  hero: Hero | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+}
 }
