b8ef7dc151b4006ddb63ffde7df12cdc99f9ee70
iYEiD <zeinoeid@gmail.com>
2025-02-08 19:15:23 +0200

    feat: add post request to add new hero

--- src/app/features/components/heroes/heroes.component.ts
+++ src/app/features/components/heroes/heroes.component.ts
@@ -29,4 +29,13 @@ getHeroes(): void {
   this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
 }
+
+add(name: string): void {
+  name = name.trim();
+  if (!name) { return; }
+  this.heroService.addHero({ name } as Hero)
+    .subscribe(hero => {
+      this.heroes.push(hero);
+    });
+}
 }
